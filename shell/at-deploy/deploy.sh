629d1effa4cea17bd32859c9398616a653e4af18
MRXia <xiazijian666@gmail.com>
2019-09-26 16:28:42 +0800

    feat(shell): modify 'at' deploy script

--- shell/at-deploy/deploy.sh
+++ shell/at-deploy/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 declare -A dirs
-dirs=(["java"]="java_micro_srvs" ["php"]="php" ["node"]="nodejs_srvs")
+dirs=(["java"]="java_micro_srvs" ["php"]="php" ["nodejs"]="nodejs_srvs")
 BASE_DIR="/home/opt/env_scripts/"
 CONFIG_DIR="data/service_config"
 DEPLOY_DIR="scripts/"
 DEPLOY_FILE="deploy.list"
 BUILD_FILE="build.py"
 
+LANG=${1-java}
+
 #change deploy tag
-FILE_NAME=${BASE_DIR}${DEPLOY_DIR}${dirs[$1]}"/"${DEPLOY_FILE}
+FILE_NAME=${BASE_DIR}${DEPLOY_DIR}${dirs[$LANG]}"/"${DEPLOY_FILE}
 echo ${2}" Now deployed :" $(sed -n '/^'${2}'/p' $FILE_NAME)
 read -p "Please input a new tag: " NEW_TAG
 sed -i 's/^'${2}'.*/'${2}': '${NEW_TAG}'/' $FILE_NAME
@@ -19,10 +21,10 @@ do
     case $deploy_now in
         [yY])
             #package image
-            python /home/opt/env_scripts/scripts/java_micro_srvs/build.py
+            python ${BASE_DIR}${DEPLOY_DIR}${dirs[$LANG]}"/"${BUILD_FILE}
 
             #build docker image
-            cd /home/opt/env_scripts/composefiles && docker-compose -f java.compose.yml up -d --build
+            cd /home/opt/env_scripts/composefiles && docker-compose -f $LANG.compose.yml up -d --build
             ;;
 
         [nN])
